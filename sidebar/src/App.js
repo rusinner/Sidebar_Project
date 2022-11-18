a59c3f19a989ef473fa2ece5a60eae828415309c
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-18 14:39:14 +0200

    functionality starts

--- sidebar/src/App.js
+++ sidebar/src/App.js
@@ -5,7 +5,13 @@ import Home from "./Home";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
 }
 
 export default App;
